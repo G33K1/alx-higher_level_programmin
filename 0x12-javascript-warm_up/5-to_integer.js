9600c59412a9cb0dd46b50d8dfc8757f8b6c89a1
G33K1 <johnjomo30@gmail.com>
2024-06-18 10:25:33 -0700

    integers

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const arg = process.argv;
 if (isNaN(arg[2])) {
   console.log('Not a number');
 } else {
-  console.log('My number is: ' + parseInt(arg[2]));
+  console.log('My number: ' + parseInt(arg[2]));
 }
