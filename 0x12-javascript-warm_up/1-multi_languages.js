35070d33a2b0af7e620c02f61339e0bc79cc5020
G33K1 <johnjomo30@gmail.com>
2024-06-18 08:21:34 -0700

    multiple print

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
-const myvar = 'C is fun\n';
-const mvar = 'Python is cool\n';
+const myvar = 'C is fun';
+const mvar = 'Python is cool';
 const myVar = 'JavaScript is amazing';
-console.log(myvar,mvar,myVar);
+console.log(myvar);
+console.log(mvar);
+console.log(myVar);
